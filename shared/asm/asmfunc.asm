3bff0df81b8d8fee6056853500a3b43705d3469e
shimazu <e5120@gm.kochi-ct.jp>
2017-11-01 00:22:41 +0900

    delete write_mem8() function

--- shared/asm/asmfunc.asm
+++ shared/asm/asmfunc.asm
@@ -1,14 +1,8 @@
 [BITS 32]         ; 32bit modeの機械語
 
-  GLOBAL    io_hlt,write_mem8   ; 関数名
+  GLOBAL    io_hlt   ; 関数名
 
 [SECTION .text]   ; オブジェクトファイルではコレを書いてからプログラム記述
 io_hlt :         ; void io_hlt(void);
   HLT
-  RET
-
-write_mem8 :      ; void write_mem8(int addr, int data);
-  MOV     ECX,[ESP+4]
-  MOV     AL,[ESP+8]
-  MOV     [ECX],AL
   RET
--- shared/src/bootpack.c
+++ shared/src/bootpack.c
@@ -1,10 +1,11 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 void HariMain(void){
   int i;
+  char* p;
   for(i = 0xa0000; i <= 0xaffff; ++i){
-    write_mem8(i, 10);
+    p = (char *)i;
+    *p = i & 0x0f;
   }
   while(1){
     io_hlt();

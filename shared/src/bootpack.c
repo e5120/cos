41a16a5821cbd9e0cf0c2e8fa48eb24874d926fe
shimazu <e5120@gm.kochi-ct.jp>
2017-10-31 23:47:55 +0900

    created chapter3

--- shared/src/bootpack.c
+++ shared/src/bootpack.c
@@ -4,7 +4,7 @@ void write_mem8(int addr, int data);
 void HariMain(void){
   int i;
   for(i = 0xa0000; i <= 0xaffff; ++i){
-    write_mem8(i, 15);
+    write_mem8(i, 10);
   }
   while(1){
     io_hlt();
